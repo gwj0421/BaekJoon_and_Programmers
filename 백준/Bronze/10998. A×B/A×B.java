0781575e5fa7aab98e0cf70ce0ec8f180445898f
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2022-03-28 14:09:06 +0900

    [Bronze V] Title: A×B, Time: 72 ms, Memory: 12836 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+public class Main {
+
+	public static void main(String[] args)throws IOException {
+		int a =System.in.read();
+		System.in.read();
+		System.out.println((a-48)*(System.in.read()-48));
+		
+		}
+	}
