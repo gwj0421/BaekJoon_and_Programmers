82bdfcb04d43c85989037784c176a40f4a5cfcf4
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2022-07-19 19:13:17 +0900

    [level 2] Title: 괄호 변환, Time: 0.21 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/60058. 괄호 변환/괄호 변환.py
@@ -0,0 +1,52 @@
+def func_2(w):
+    left,right=0,0
+    for i in range(len(w)):
+        if w[i]=='(':
+            left+=1
+        else:
+            right+=1
+        if left==right:
+            u=w[:i+1]
+            v=w[i+1:]
+            return u,v
+
+def isRight(temp):
+    stack=[]
+    stack.append(temp[0])
+    for i in temp[1:]:
+        if stack[-1:]==['('] and i==')':
+            stack.pop()
+        else:
+            stack.append(i)
+    if stack:
+        return False
+    else:
+        return True
+    
+def solution(p):
+    # func_1
+    if not p:
+        return ""
+    
+    # func_2
+    u,v=func_2(p)
+    
+    # func_3
+    if isRight(u):
+        # func_3-1
+        return u+solution(v)
+    else:
+        # func_4-1
+        answer="("
+        # func_4-2
+        answer+=solution(v)
+        # func_4-3
+        answer+=")"
+        # func_4-4
+        for i in u[1:-1]:
+            if i=="(":
+                answer+=")"
+            else:
+                answer+="("
+        # func_4-5
+        return answer
