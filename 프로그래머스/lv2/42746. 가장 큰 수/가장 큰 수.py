4814592b43506904d81862c4e73b7d17ed221dee
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2022-11-04 22:31:41 +0900

    [level 2] Title: 가장 큰 수, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,4 +1,19 @@
+# def solution(numbers):
+#     numbers = list(map(str, numbers))
+#     numbers.sort(key=lambda x: x*3, reverse=True)
+#     return str(int(''.join(numbers)))
+import functools
+def cal(a,b):
+    a1 = int(a+b)
+    a2 = int(b+a)
+    if a1>a2 :
+        return 1
+    elif a1==a2:
+        return 0
+    else:
+        return -1
+
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x*3, reverse=True)
-    return str(int(''.join(numbers)))
+    temp = list(map(str,numbers))
+    temp.sort(reverse=True,key=functools.cmp_to_key(cal))
+    return str(int(''.join(temp)))
