e0c0b1dba3cda3d29c761361be5eee1ce01fea12
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2023-02-04 20:06:20 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 824 ms, Memory: 120264 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.py
@@ -1,13 +1,24 @@
-#10816
-from collections import Counter
-n=int(input())
-nlist=Counter(list(map(int,input().split())))
-m=int(input())
-mlist=list(map(int,input().split()))
-result=[]
-for i in range(m):
-    temp=nlist.get(mlist[i])
-    if temp==None:
-        temp=0
-    result.append(temp)
-print(*result,sep=' ')
+import sys
+
+
+def solution():
+    input = sys.stdin.readline
+    n = int(input().rstrip('\n'))
+    numCards = list(map(int, input().split()))
+    m = int(input().rstrip('\n'))
+    questions = list(map(int, input().split()))
+
+    cnt = {x: 0 for x in list(set(numCards))}
+    ans = []
+    for i in numCards:
+        cnt[i] += 1
+
+    for q in questions:
+        if cnt.get(q):
+            ans.append(cnt.get(q))
+        else:
+            ans.append(0)
+    print(*ans)
+
+
+solution()
