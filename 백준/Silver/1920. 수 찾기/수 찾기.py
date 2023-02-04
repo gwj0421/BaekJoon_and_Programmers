bf1e3e5985c4f2807e3457a803aaac93eb0af953
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2023-02-04 20:20:15 +0900

    [Silver IV] Title: 수 찾기, Time: 160 ms, Memory: 50416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,26 +1,18 @@
-#1920
-testnum=int(input())
-testlist=list(map(int,input().split()))
-testlist.sort()
-
-resultnum=int(input())
-resultlist=list(map(int,input().split()))
-
-answer=[0 for i in range(resultnum)]
+import sys
 
 
-for i in range(resultnum):
-    target=resultlist[i]
-    low=0
-    high=testnum-1
-    while low<=high:
-        mid=(low+high)//2
-        if testlist[mid]==target:
-            answer[i]=1
-            break
-        elif testlist[mid]<target:
-            low=mid+1
+def solution():
+    input = sys.stdin.readline
+    n = int(input().rstrip('\n'))
+    nums = set(list(map(int, input().split())))
+    m = int(input().rstrip('\n'))
+    questions = list(map(int, input().split()))
+
+    for q in questions:
+        if q in nums:
+            print(1)
         else:
-            high=mid-1
-for i in answer:
-    print(i)
+            print(0)
+
+
+solution()
