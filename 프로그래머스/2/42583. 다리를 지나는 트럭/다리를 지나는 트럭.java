a6f8276d1748142e6403a4587badeef72bdaa60e
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2023-11-30 18:19:39 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.71 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -2,34 +2,68 @@ import java.util.LinkedList;
 import java.util.Queue;
 
 class Solution {
+    class Truck {
+        private final int weight;
+        private int pos;
+
+        public Truck(int weight) {
+            this.weight = weight;
+            this.pos = 1;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public int getPos() {
+            return pos;
+        }
+
+        public void move() {
+            this.pos++;
+        }
+    }
 
     public int solution(int bridge_length, int weight, int[] truck_weights) {
         int time = 0;
-        Queue<Integer> onBridge = new LinkedList<>();
+        Queue<Truck> waitingTruck = initWaitingTruck(truck_weights);
+        Queue<Truck> onBridge = new LinkedList<>();
         int onBridgeWeight = 0;
-        for (int truck : truck_weights) {
-            while (true) {
-                if (onBridge.isEmpty()) {
-                    onBridge.offer(truck);
-                    onBridgeWeight += truck;
-                    time++;
-                    break;
-                } else if (onBridge.size() == bridge_length) {
-                    onBridgeWeight -= onBridge.poll();
-                } else {
-                    if (onBridgeWeight + truck > weight) {
-                        onBridge.offer(0);
-                        time++;
-                    } else {
-                        onBridge.offer(truck);
-                        onBridgeWeight += truck;
-                        time++;
-                        break;
-                    }
-                }
+
+        while (!waitingTruck.isEmpty() || !onBridge.isEmpty()) {
+            time++;
+            if (onBridge.isEmpty()) {
+                Truck truck = waitingTruck.poll();
+                onBridge.add(truck);
+                onBridgeWeight += truck.getWeight();
+                continue;
             }
+
+            for (Truck truckOnBridge : onBridge) {
+                truckOnBridge.move();
+            }
+
+            if (onBridge.peek().getPos() > bridge_length) {
+                Truck outTruck = onBridge.poll();
+                onBridgeWeight -= outTruck.getWeight();
+            }
+
+            if (!waitingTruck.isEmpty() && waitingTruck.peek().getWeight() + onBridgeWeight <= weight) {
+                Truck truck = waitingTruck.poll();
+                onBridgeWeight += truck.getWeight();
+                onBridge.offer(truck);
+            }
+
         }
-        return time + bridge_length;
+        return time;
+    }
+
+    private Queue<Truck> initWaitingTruck(int[] truck_weights) {
+        Queue<Truck> result = new LinkedList<>();
+        for (int truckWeight : truck_weights) {
+            result.offer(new Truck(truckWeight));
+        }
+        return result;
     }
 
 }
