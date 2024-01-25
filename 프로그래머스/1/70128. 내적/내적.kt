65dfdefefb0cdbde945488cb729a11e59fabfd89
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2024-01-25 16:05:35 +0900

    [level 1] Title: 내적, Time: 22.86 ms, Memory: 64 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.kt
+++ 프로그래머스/1/70128. 내적/내적.kt
@@ -1,9 +1,3 @@
 class Solution {
-    fun solution(a: IntArray, b: IntArray): Int {
-        var answer: Int = 0
-        for (i in a.indices) {
-            answer += a[i] * b[i]
-        }
-        return answer
-    }
+    fun solution(a: IntArray, b: IntArray): Int = a.zip(b).map { it.first * it.second }.sum()
 }
