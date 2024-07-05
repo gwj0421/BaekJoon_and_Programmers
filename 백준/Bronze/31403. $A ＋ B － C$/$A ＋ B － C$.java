045a1bd751bd3fdea7d9ed4fb4f5ee91547fa39f
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2024-07-05 14:17:03 +0900

    [Bronze IV] Title: $A + B - C$, Time: 208 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/31403. $A ＋ B － C$/$A ＋ B － C$.java
@@ -0,0 +1,16 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+//        File file = new File("./input.txt");
+//
+//        Scanner sc = new Scanner(file);
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String c = sc.next();
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c));
+        System.out.println(Integer.parseInt(a + b) - Integer.parseInt(c));
+    }
+}
