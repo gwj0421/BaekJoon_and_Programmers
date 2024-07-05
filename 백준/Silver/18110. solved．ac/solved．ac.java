1f5fa4d869c3966f2bdff99a784d41041b4ca247
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2024-07-05 15:51:09 +0900

    [Silver IV] Title: solved.ac, Time: 284 ms, Memory: 32644 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -1,35 +1,61 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 //        File file = new File("./input.txt");
 //
 //        Scanner sc = new Scanner(file);
-//        Scanner sc = new Scanner(System.in);
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         if (n == 0) {
             System.out.println(0);
             return;
         }
-        int[] score = new int[n];
+        int[] score = new int[31];
         for (int i = 0; i < n; i++) {
-            score[i] = Integer.parseInt(br.readLine());
+            score[Integer.parseInt(br.readLine())]++;
         }
-        showTrimmedAverage(score);
+        showTrimmedAverage(n, score);
     }
 
-    public static void showTrimmedAverage(int[] score) {
-        Arrays.sort(score);
-        int dropCnt = Math.round(score.length * 0.15F);
-        float sum = 0;
-        for (int i = dropCnt; i < score.length - dropCnt; i++) {
-            sum += score[i];
+    public static void showTrimmedAverage(int n, int[] score) {
+        int dropCnt = Math.round(n * 0.15F);
+
+        int leftIdx = 0;
+        int leftDropCnt = 0;
+        while (leftDropCnt < dropCnt) {
+            if (score[leftIdx] > 0) {
+                score[leftIdx]--;
+                leftDropCnt++;
+            } else {
+                leftIdx++;
+            }
         }
-        System.out.println(Math.round(sum / (score.length - 2 * dropCnt)));
+
+        int rightIdx = score.length - 1;
+        int rightDropCnt = 0;
+        while (rightDropCnt < dropCnt) {
+            if (score[rightIdx] > 0) {
+                score[rightIdx]--;
+                rightDropCnt++;
+            } else {
+                rightIdx--;
+            }
+        }
+
+        int sum = 0;
+        int cnt = 0;
+        for (int i = 1; i < score.length; i++) {
+            if (score[i] > 0) {
+                sum += i * score[i];
+                cnt += score[i];
+            }
+        }
+        System.out.println(Math.round((float) sum/ cnt));
+
     }
 
 }
