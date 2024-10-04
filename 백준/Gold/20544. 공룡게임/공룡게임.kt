409f910e4b96911cd45c916b49107be9a11fc04b
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2024-10-04 18:02:01 +0900

    [Gold IV] Title: 공룡게임, Time: 72 ms, Memory: 12204 KB -BaekjoonHub

--- 백준/Gold/20544. 공룡게임/공룡게임.kt
+++ 백준/Gold/20544. 공룡게임/공룡게임.kt
@@ -1,32 +1,29 @@
 package org.example
 
-import java.io.BufferedReader
-import java.io.FileReader
-import java.util.LinkedList
-import java.util.PriorityQueue
-import java.util.Queue
-import kotlin.math.abs
-import kotlin.math.cos
-import kotlin.math.max
-import kotlin.math.min
-
 fun main()
 = with(System.`in`.bufferedReader())
 //        = with(BufferedReader(FileReader("./src/main/resources/input.txt")))
 {
     val mod: Int = 1_000_000_007
+    val n: Int = readLine().toInt()
 
-    val n = readLine().toInt()+1
-
-    val dp = Array(1002, { LongArray(2) })
-    dp[0][1] = 1
-    dp[1][0] = 1
-    dp[2][0] = 1
-    dp[3] = longArrayOf(2, 1)
-
-    for (i: Int in 4..n) {
-        dp[i][0] = (dp[i - 1][0] + dp[i - 2][0] + dp[i - 3][0]) % mod
-        dp[i][1] = (dp[i - 1][1] + dp[i - 2][0] + dp[i - 2][1] * 2 + dp[i - 3][0] * 2 + dp[i - 3][1] * 3) % mod
+    if (n < 2) {
+        println(0)
+        return
+    }
+    val dp:Array<LongArray> = Array(n+1,{ LongArray(2)})
+    dp[0] = longArrayOf(1,0)
+    dp[1] = longArrayOf(1,0)
+    dp[2] = longArrayOf(2,1)
+    for (i: Int in 3..n) {
+        dp[i][0] = (
+                dp[i-1][0] + dp[i-2][0] + dp[i-3][0]
+                ) % mod
+        dp[i][1] = (
+                dp[i-1][1] +
+                        dp[i-2][0] +dp[i-2][1]*2 +
+                        dp[i-3][0]*2 + dp[i-3][1]*3
+                ) % mod
     }
     println(dp[n][1])
 }
