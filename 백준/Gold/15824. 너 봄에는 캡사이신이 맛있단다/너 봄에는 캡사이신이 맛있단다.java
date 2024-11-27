542a25d8720c9217c4a2e45957370a446da361e9
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2024-11-27 13:32:57 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 472 ms, Memory: 15420 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Main {
+    //1 4 5 5 6 10
+    //1 4 =>1
+    //1 5 =>2
+    //1 5 =>4
+    private static final int MOD = 1_000_000_007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
+        int n = Integer.parseInt(br.readLine());
+        List<Long> menus = Arrays.stream(br.readLine().split(" ")).map(Long::parseLong).sorted().collect(Collectors.toList());
+        long ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                long diff = menus.get(j) - menus.get(i);
+                ans = (ans + diff * pow(j - i - 1) % MOD)%MOD;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static long pow(int superscript) {
+        if (superscript == 0) {
+            return 1;
+        }
+        long temp = pow(superscript / 2);
+        temp = temp * temp % MOD;
+        if (superscript % 2 == 1) {
+            return temp * 2 % MOD;
+        }
+        return temp;
+    }
+
+}
+
+
