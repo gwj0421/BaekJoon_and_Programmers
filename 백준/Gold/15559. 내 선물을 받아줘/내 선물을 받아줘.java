2d2520ceacf2d44d22a8866c3c0d931d0618b31e
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2024-11-29 22:41:26 +0900

    [Gold II] Title: 내 선물을 받아줘, Time: 324 ms, Memory: 49672 KB -BaekjoonHub

--- 백준/Gold/15559. 내 선물을 받아줘/내 선물을 받아줘.java
+++ 백준/Gold/15559. 내 선물을 받아줘/내 선물을 받아줘.java
@@ -1,18 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static final Map<Character, int[]> MOVE = Map.of(
-            'N', new int[]{-1, 0},
-            'S', new int[]{1, 0},
-            'W', new int[]{0, -1},
-            'E', new int[]{0, 1}
-    );
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        BufferedReader br = new BufferedReader(new FileReader("./input.txt"));
@@ -34,47 +26,49 @@ public class Main {
         private final int n;
         private final int m;
         private final char[][] appleMovePattern;
+        private final int[][] visit;
+        private int ans;
 
         public SearchApple(int n, int m, char[][] appleMovePattern) {
             this.n = n;
             this.m = m;
             this.appleMovePattern = appleMovePattern;
+            this.visit = new int[n][m];
+            this.ans = 0;
+
         }
 
         public void search() {
-            int circleType = 0;
-            int[][] visit = new int[n][m];
-            for (int i = 0; i < n; i++) {
-                Arrays.fill(visit[i], -1);
-            }
+            int circleType = 1;
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
-                    if (visit[i][j] < 0) {
-                        visit[i][j] = circleType;
-                        visit[i][j] = dfs(visit, new Coord(i, j), circleType);
-                        if (visit[i][j] == circleType) {
-                            circleType++;
-                        }
+                    if (visit[i][j] == 0) {
+                        dfs(new Coord(i, j), circleType++);
                     }
                 }
             }
-            System.out.println(circleType);
+            System.out.println(ans);
 
         }
 
-        private int dfs(int[][] visit, Coord now, int circleType) {
+        private void dfs(Coord now, int circleType) {
+            visit[now.y][now.x] = circleType;
             Coord next = now.getNextCoord(appleMovePattern[now.y][now.x]);
-            if (visit[next.y][next.x] < 0) {
-                visit[next.y][next.x] = circleType;
-                return visit[next.y][next.x] = dfs(visit, next, circleType);
-            } else if (visit[next.y][next.x] != circleType) {
-                return visit[next.y][next.x];
+            if (visit[next.y][next.x] == 0) {
+                dfs(next, circleType);
+            } else if (visit[next.y][next.x] == circleType) {
+                ans++;
             }
-            return circleType;
         }
     }
 
     static class Coord {
+        private static final Map<Character, int[]> MOVE = Map.of(
+                'N', new int[]{-1, 0},
+                'S', new int[]{1, 0},
+                'W', new int[]{0, -1},
+                'E', new int[]{0, 1}
+        );
         private final int y;
         private final int x;
 
@@ -90,14 +84,5 @@ public class Main {
             );
         }
 
-        @Override
-        public String toString() {
-            return "Coord{" +
-                    "y=" + y +
-                    ", x=" + x +
-                    '}';
-        }
     }
 }
-
-
