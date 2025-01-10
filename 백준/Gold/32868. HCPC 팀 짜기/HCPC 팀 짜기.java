f4919c525122627c6dbfc78d0847526610b7d184
Cho gunwoong <80460636+gwj0421@users.noreply.github.com>
2025-01-10 23:06:49 +0900

    [Gold II] Title: HCPC 팀 짜기, Time: 724 ms, Memory: 118576 KB -BaekjoonHub

--- 백준/Gold/32868. HCPC 팀 짜기/HCPC 팀 짜기.java
+++ 백준/Gold/32868. HCPC 팀 짜기/HCPC 팀 짜기.java
@@ -20,11 +20,7 @@ public class Main {
         Map<Integer, List<Integer>> tailCntMap = new HashMap<>();
         for (int i = 1; i <= n; i++) {
             required[i] = Integer.parseInt(st.nextToken());
-            if (tailCntMap.containsKey(required[i])) {
-                tailCntMap.get(required[i]).add(i);
-            } else {
-                tailCntMap.put(required[i], new ArrayList<>(List.of(i)));
-            }
+            tailCntMap.computeIfAbsent(required[i], k -> new ArrayList<>()).add(i);
         }
 
         Solution solution = new Solution(n, required, tailCntMap);
@@ -32,99 +28,221 @@ public class Main {
     }
 
     static class Solution {
-        private static final int MOD = 1_000_000_007;
         private final int n;
         private final int[] required;
-        private final Map<Integer, List<Integer>> tailCntMap;
+        private final Map<Integer, List<Integer>> tailInfos;
 
         private long ans;
 
-
-        public Solution(int n, int[] required, Map<Integer, List<Integer>> tailCntMap) {
+        public Solution(int n, int[] required, Map<Integer, List<Integer>> tailInfos) {
             this.n = n;
             this.required = required;
-            this.tailCntMap = tailCntMap;
+            this.tailInfos = tailInfos;
             this.ans = 0;
         }
 
+        /**
+         * 팀 성립 조건
+         * 1. (1cycle 3개)
+         * 2. (꼬리 1개 달린 1cycle 1개) + (1cycle 1개)
+         * 3. (병렬 2개 꼬리 달린 1cycle 1개)
+         * 4. (직렬 2개 꼬리 달린 1cycle 1개)
+         * 5. (2cycle 1개) + (1cycle 1개)
+         * 6. (꼬리 1개 달린 2cycle 1개)
+         * 7. (3cycle 1개)
+         * <p>
+         * 그럼으로 필요한 변수와 사용 조건
+         * - 전체 1cycle들의 총 개수 => 1번, 2번, 5번 조건
+         * - 전체 1cycle들의 꼬리 개수 => 2번 , 3번 조건
+         * - 전체 1cycle들의 꼬리의 꼬리의 개수 => 4번 조건
+         * - 전체 2cycle들의 총 개수 => 5번 조건
+         * - 전체 2cycle들의 꼬리 개수 => 6번 조건
+         * - 전체 3cycle들의 총 개수 => 7번 조건
+         */
+
         public void solve() {
-            // 전체 solo,
-            int[] solo = new int[3];
-            int[] duo = new int[2];
+            SoloCycleCondition soloCycleCondition = new SoloCycleCondition(required, tailInfos);
+            DuoCycleCondition duoCycleCondition = new DuoCycleCondition(required, tailInfos);
+            TrioCycleCondition trioCycleCondition = new TrioCycleCondition(required, tailInfos);
             boolean[] visit = new boolean[n + 1];
-            int trioCnt = 0;
             for (int i = 1; i <= n; i++) {
                 if (!visit[i]) {
-                    if (isSolo(i)) {
-                        visit[i] = true;
-                        solo[0] = (solo[0] + 1) % MOD;
-                        if (tailCntMap.get(i).size() > 1) {
-                            solo[1] = (solo[1] + tailCntMap.get(i).size() - 1) % MOD;
-                            // 3번
-                            ans = (ans + nC2(tailCntMap.get(i).size() - 1)) % MOD;
-                            // 4번
-                            for (int firstTail : tailCntMap.get(i)) {
-                                if (firstTail != i && tailCntMap.containsKey(firstTail)) {
-                                    ans = (ans + tailCntMap.get(firstTail).size() % MOD) % MOD;
-                                }
-                            }
-                        }
-                    } else if (isDuo(i)) {
-                        visit[i] = true;
-                        visit[required[i]] = true;
-                        duo[0] = (duo[0] + 1) % MOD;
-                        if (tailCntMap.get(i).size() > 1 || tailCntMap.get(required[i]).size() > 1) {
-                            duo[1] = (duo[1] + tailCntMap.get(i).size() + tailCntMap.get(required[i]).size() - 2) % MOD;
-                        }
-                    } else if (isTrio(i)) {
-                        visit[i] = true;
-                        visit[required[i]] = true;
-                        visit[required[required[i]]] = true;
-                        trioCnt = (trioCnt + 1) % MOD;
+                    if (soloCycleCondition.canActivate(i)) {
+                        soloCycleCondition.processVisit(visit, i, 1);
+                        soloCycleCondition.processTail(i);
+                        soloCycleCondition.processParallel(i);
+                        soloCycleCondition.processSerial(i);
+                    } else if (duoCycleCondition.canActivate(i)) {
+                        duoCycleCondition.processVisit(visit, i, 2);
+                        duoCycleCondition.processTail(i);
+                    } else if (trioCycleCondition.canActivate(i)) {
+                        trioCycleCondition.processVisit(visit, i, 3);
                     }
                 }
             }
 
             // 1번
-            ans = (ans + nC3(solo[0])) % MOD;
+            ans = NumberUtils.plusValue(ans, NumberUtils.nC3(soloCycleCondition.getTotalCnt()));
             // 2번
-            ans = (ans + (long) (solo[0] - 1) * solo[1] % MOD) % MOD;
+            ans = NumberUtils.plusValue(ans, NumberUtils.multiValue(soloCycleCondition.getTotalCnt() - 1, soloCycleCondition.getTailCnt()));
+            // 3번
+            ans = NumberUtils.plusValue(ans, soloCycleCondition.getParallelTailCnt());
+            // 4번
+            ans = NumberUtils.plusValue(ans, soloCycleCondition.getSerialTailCnt());
             // 5번
-            ans = (ans + (long) duo[0] * solo[0] % MOD) % MOD;
+            ans = NumberUtils.plusValue(ans, NumberUtils.multiValue(duoCycleCondition.getTotalCnt(), soloCycleCondition.getTotalCnt()));
             // 6번
-            ans = (ans + duo[1]) % MOD;
+            ans = NumberUtils.plusValue(ans, duoCycleCondition.getTailCnt());
             // 7번
-            ans = (ans + trioCnt) % MOD;
+            ans = NumberUtils.plusValue(ans, trioCycleCondition.getTotalCnt());
 
             System.out.println(ans);
-
-
         }
+    }
+}
 
-        private boolean isSolo(int i) {
-            return i == required[i];
+class NumberUtils {
+    private static final int MOD = 1_000_000_007;
+
+    public static long nC2(long value) {
+        if (value < 2) {
+            return 0;
         }
+        return value * (value - 1) / 2;
+    }
 
-        private boolean isDuo(int i) {
-            return i == required[required[i]];
+    public static long nC3(long value) {
+        if (value < 3) {
+            return 0;
         }
+        return value * (value - 1) * (value - 2) / 6;
+    }
 
-        private boolean isTrio(int i) {
-            return i == required[required[required[i]]];
-        }
+    public static long plusValue(long target, long value) {
+        return (target + value % MOD) % MOD;
+    }
 
-        private int nC2(long value) {
-            if (value < 2) {
-                return 0;
+    public static long multiValue(long target, long value) {
+        return target * value % MOD;
+    }
+}
+
+class SoloCycleCondition extends Condition {
+    private long tailCnt;
+    private long parallelTailCnt;
+    private long serialTailCnt;
+
+    public SoloCycleCondition(int[] heads, Map<Integer, List<Integer>> tailInfos) {
+        super(heads, tailInfos);
+        this.tailCnt = 0;
+        this.parallelTailCnt = 0;
+        this.serialTailCnt = 0;
+    }
+
+    public long getTailCnt() {
+        return tailCnt;
+    }
+
+    public long getParallelTailCnt() {
+        return parallelTailCnt;
+    }
+
+    public long getSerialTailCnt() {
+        return serialTailCnt;
+    }
+
+    @Override
+    public boolean canActivate(int start) {
+        return start == getHead(start, 1);
+    }
+
+    public void processTail(int start) {
+        this.tailCnt = NumberUtils.plusValue(this.tailCnt, getTail(start).size() - 1);
+    }
+
+    public void processParallel(int start) {
+        this.parallelTailCnt = NumberUtils.plusValue(this.parallelTailCnt, NumberUtils.nC2(getTail(start).size() - 1));
+    }
+
+    public void processSerial(int start) {
+        for (int firstTail : getTail(start)) {
+            if (firstTail != start) {
+                this.serialTailCnt = NumberUtils.plusValue(this.serialTailCnt, getTail(firstTail).size());
             }
-            return (int) (value * (value - 1) / 2 % MOD);
         }
+    }
+}
 
-        private int nC3(long value) {
-            if (value < 3) {
-                return 0;
-            }
-            return (int) (value * (value - 1) * (value - 2) / 6 % MOD);
+class DuoCycleCondition extends Condition {
+    private long tailCnt;
+
+    public DuoCycleCondition(int[] heads, Map<Integer, List<Integer>> tailInfos) {
+        super(heads, tailInfos);
+        this.tailCnt = 0;
+    }
+
+    public long getTailCnt() {
+        return tailCnt;
+    }
+
+    @Override
+    public boolean canActivate(int start) {
+        return start == getHead(start, 2);
+    }
+
+    public void processTail(int start) {
+        this.tailCnt = NumberUtils.plusValue(this.tailCnt, getTail(start).size() + getTail(getHead(start, 1)).size() - 2);
+    }
+}
+
+class TrioCycleCondition extends Condition {
+    public TrioCycleCondition(int[] heads, Map<Integer, List<Integer>> tailInfos) {
+        super(heads, tailInfos);
+    }
+
+    @Override
+    public boolean canActivate(int start) {
+        return start == getHead(start, 3);
+    }
+}
+
+abstract class Condition {
+    private final int[] heads;
+    private final Map<Integer, List<Integer>> tailInfos;
+    private long totalCnt;
+
+    public Condition(int[] heads, Map<Integer, List<Integer>> tailInfos) {
+        this.heads = heads;
+        this.tailInfos = tailInfos;
+        this.totalCnt = 0;
+    }
+
+    public long getTotalCnt() {
+        return totalCnt;
+    }
+
+    abstract public boolean canActivate(int start);
+
+    public void processVisit(boolean[] visit, int start, int depth) {
+        for (int i = 0; i < depth; i++) {
+            visit[start] = true;
+            start = getHead(start, 1);
         }
+        count();
+    }
+
+    private void count() {
+        this.totalCnt = NumberUtils.plusValue(this.totalCnt, 1);
+    }
+
+    public int getHead(int node, int depth) {
+        for (int i = 0; i < depth; i++) {
+            node = heads[node];
+        }
+        return node;
+    }
+
+    public List<Integer> getTail(int start) {
+        return tailInfos.computeIfAbsent(start, k -> List.of());
     }
 }
